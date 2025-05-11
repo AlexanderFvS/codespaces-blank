a425b6ee81dabe456558d6377c22e2ac3d2cd3f2
Joaquin Jimenez Cedron <142637048+joajced@users.noreply.github.com>
2025-05-11 23:37:30 +0200

    Update encoder.h

--- encoder.h
+++ encoder.h
@@ -1,33 +1,26 @@
-#ifndef ENCODERM_H
-#define ENCODERM_H
+#ifndef ENCODER_H
+#define ENCODER_H
 
-#include "stm32f4xx_hal.h"
+typedef enum
+{
+	PHASE_A = 0x00u,
+	PHASE_B = 0x10u,
+	PHASE_C = 0x11u,
+	PHASE_D = 0x01u
+} Phase;
 
-int count = 0;
+typedef enum
+{
+	FORWARDS,
+	BACKWARDS,
+	INVALID,
+	NO_CHANGE
+} Direction;
 
-typedef enum {
-STAT_F,							// Vorwaerts
-STAT_B, 						// Rueckwaerts
-STAT_E, 						// Error
-STAT_N} 						// Nichts
-Status; 
+int getPhase();
 
-typedef enum {
-PHASE_A = 0,				// =00
-PHASE_B = 2,				// =10
-PHASE_C = 3, 				// =11
-PHASE_D = 1} 				// =01
-Phase;
+int getCount();
 
-Status state_sieve[4][4] = {
-[PHASE_A] = {STAT_N, STAT_B, STAT_F, STAT_E},   // A - D - B - C 
-[PHASE_B]	= {STAT_B, STAT_E, STAT_N, STAT_F}, 
-[PHASE_C] = {STAT_E, STAT_F, STAT_B, STAT_N},
-[PHASE_D] = {STAT_F, STAT_N, STAT_E, STAT_B}};
+void resetCount();
 
-
-int readM ();
-
-void resetCount ();
-
-#endif 
+#endif
