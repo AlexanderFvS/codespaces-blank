4a7ecad44407491369a847dc2ee43bdec3eac37b
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-05-12 15:07:16 +0200

    Update main.c
    setPhase vor schleife und setPhase nach error um fehler zu vermeiden.

--- Src/main.c
+++ Src/main.c
@@ -8,6 +8,7 @@
 #include "angle.h"
 #include "timestamp.h"
 #include "led_io.h"
+#include "delay.h"
 
 #define ZEITFENSTER 0.5
 
@@ -25,6 +26,9 @@ int main()
 	double geschw1 = 0.0;
 	printWinkel(winkel1);
 	printGeschw(geschw1);
+	setPhase();
+	
+	//____________________________________Superloop____________________________________________//
 	
 	while (1)
 	{
@@ -33,20 +37,29 @@ int main()
 			printError();
 			while (!readButtonF(6));
 			clearError();
+			//delay(20);
+			setPhase();
 		}
 		
 		uint32_t time2 = getTimeM();
-		int count2 = getCount();
 		double period = getPeriodM(time1, time2);
 		
 		if (period > ZEITFENSTER)
 		{
+			int count2 = getCount();
+			
 			double winkel2 = calcWinkel();
 			double geschw2 = calcGeschw(count1, count2, period);
 			
-			if (winkel2 != winkel1) printWinkel(winkel2);
-			if (geschw2 != geschw1) printGeschw(geschw2);
-			
+			//GPIOE->BSRR = (0x01U);																		// Oszilloskop
+			if (winkel2 != winkel1) { 
+				printWinkel(winkel2);
+			}
+			 if (geschw2 != geschw1) {
+				 printGeschw(geschw2);
+			 }
+			//GPIOE->BSRR = (0x01U << 16);															// Oszilloskop
+			 
 			count1 = count2;
 			time1 = time2;
 			winkel1 = winkel2;
