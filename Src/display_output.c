003e725765d5642bd08c151a7be82a56e5d8ced9
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-05-12 15:09:44 +0200

    Update display_output.c
    grad und sec in Labels
    printReplS statt PrintS
    %5.1f läuft viel stabiler da er nicht zu viele nachkommastellen dazu wirft

--- Src/display_output.c
+++ Src/display_output.c
@@ -5,10 +5,10 @@
 void printLabels()
 {
 	lcdGotoXY(1,1);
-	lcdPrintS("Drehwinkel:");
+	lcdPrintS("Drehwinkel in Grad");
 	
 	lcdGotoXY(1,4);
-	lcdPrintS("Drehgesch.:");
+	lcdPrintS("Drehgesch. in Grad/Sek.");
 }
 
 void printError()
@@ -20,23 +20,21 @@ void printError()
 void clearError()
 {
 	lcdGotoXY(1, 8);
-	lcdPrintS(" ");
+	lcdPrintS(" ");																						// Funktion löscht alles rechts vom Cursor
 }
 
 void printWinkel(double winkel)
 {
 	char temp[12];
-	sprintf(temp, "%.2f", winkel);
+	sprintf(temp, "%5.1f", winkel);														// 5.1 reserviert 5 chars vor dem komme und 1 Nachkommastelle, da die Berechnung nicht mehr darstellt
 	lcdGotoXY(1,2);
-	lcdPrintS(temp);
-	lcdPrintS(" Grad");
+	lcdPrintReplS(temp);
 }
 
 void printGeschw(double geschw)
 {
 	char temp[12];
-	sprintf(temp, "%.2f", geschw);
+	sprintf(temp, "%5.1f", geschw);
 	lcdGotoXY(1,5);
-	lcdPrintS(temp);
-	lcdPrintS(" Grad/Sek.");
+	lcdPrintReplS(temp);
 }
