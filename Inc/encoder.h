8cb72c149f9526051cd7be02dec7f5a409787336
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-05-13 13:19:14 +0200

    Update encoder.h
    kemmentiert

--- Inc/encoder.h
+++ Inc/encoder.h
@@ -3,7 +3,7 @@
 
 typedef enum
 {
-	PHASE_A = 0x00u,
+	PHASE_A = 0x00u,											// Hier werden die Phasen mit einem Enum in der richtigen Reihenfolge benannt.
 	PHASE_B = 0x10u,
 	PHASE_C = 0x11u,
 	PHASE_D = 0x01u
@@ -11,17 +11,46 @@ typedef enum
 
 typedef enum
 {
-	FORWARDS,
+	FORWARDS,															// Hier werden alle 4 States mit einem Enum definiert.
 	BACKWARDS,
 	INVALID,
 	NO_CHANGE
 } Direction;
 
+/*
+ ****************************************************************************************
+ *  @brief      Liest die aktuelle Phase ein und vergleicht sie mit der letzten Phase.
+ *              Verändert je nacht State den Counter und schaltet die entsprechenden LEDs an.
+ *
+*  @return     int: returnt 0 wenn ok und -1 bei Error State.
+ ****************************************************************************************/
+
 int getPhase();
+
+/*
+ ****************************************************************************************
+ *  @brief      Liefert den aktuellen Wert von Count.
+ *
+*  @return     int: Count.
+ ****************************************************************************************/
 
 int getCount();
 
+/*
+ ****************************************************************************************
+ *  @brief      Setzt den Counter wieder auf 0.
+ *
+ *  @return     void
+ ****************************************************************************************/
+
 void resetCount();
+
+/*
+ ****************************************************************************************
+ *  @brief      Liest die aktuelle Phase aus und speichert sie in currPhase.
+ *
+ *  @return     void
+ ****************************************************************************************/
 
 void setPhase();
 
