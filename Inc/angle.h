913513d6a37090fc5bb16b6c8352f64745b81f6d
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-05-13 13:02:25 +0200

    Update angle.h
    kommentiert

--- Inc/angle.h
+++ Inc/angle.h
@@ -1,8 +1,23 @@
 #ifndef ANGLE_H
 #define ANGLE_H
 
+
+/*
+ ****************************************************************************************
+ *  @brief      Berechnet den Winkel in Grad.
+ *
+ *  @return     double: grad
+ ****************************************************************************************/
+ 
 double calcWinkel();
 
+/*
+ ****************************************************************************************
+ *  @brief      Berechnet die Drehgeschwindigkeit in Winkel/s.
+ *
+ *  @return     double: grad/ sekunde
+ ****************************************************************************************/
+ 
 double calcGeschw(int count1, int count2, double periode);
 
 #endif
