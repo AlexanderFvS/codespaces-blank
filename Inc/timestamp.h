c885d3a67907b4b8c2f37095cc49f77d3fb65909
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-05-13 13:37:14 +0200

    Update timestamp.h

--- Inc/timestamp.h
+++ Inc/timestamp.h
@@ -3,9 +3,34 @@
 
 #include <stm32f4xx_hal.h>
 
+
+/*
+ ****************************************************************************************
+ *  @brief      Initialisierung der Clock.
+ *
+ *  @return     void
+ ****************************************************************************************/
+ 
 void initTimeM();
+
+/*
+ ****************************************************************************************
+ *  @brief      Liefert den aktuellen Zeitstempel.
+ *
+ *  @return     uint32_t: Zeitstempel
+ ****************************************************************************************/
 	
 uint32_t getTimeM();
+
+/*
+ ****************************************************************************************
+ *  @brief      Berechnet die Zeit zwischen zwei Zeitstempel.
+ *
+ *	@param  		t1: uint32_t Zeitstempel 1
+ *	@param 			t2: uint32_t Zeitstempel 2
+ *
+ *  @return     double: Zeitdifferent
+ ****************************************************************************************/
 
 double getPeriodM(uint32_t , uint32_t);
 
