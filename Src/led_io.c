09e58926c332e5e51edf35b5abb8e79f3efb2551
AlexanderFvS <Alexander.FreiherrvonSeld@haw-hamburg.de>
2025-05-19 21:03:04 +0200

    Update led_io.c

--- Src/led_io.c
+++ Src/led_io.c
@@ -3,25 +3,31 @@
 #include <stdbool.h>
 #include "stm32f4xx_hal.h"
 
+
+
 void setLedD(int mask)
 {
 	GPIOD->ODR = mask & 0xFFu;
 }
+
 
 void resetLedD()
 {
 	GPIOD->ODR = 0;
 }
 
+
 void setLedE(int offset)
 {
 	GPIOE->ODR = 0x01u << offset;
 }
 
+
 int readLedF()
 {
 	return GPIOF->IDR & 0x03u;
 }
+
 
 bool readButtonF(int offset)
 {
